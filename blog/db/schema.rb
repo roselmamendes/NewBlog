0203020b7e2936e317e9ecb638ad4bf265b9424d
roselmamendes <roselma.mendes@gmail.com>
2014-08-24 15:30:44 -0300

    Adicionados os arquivos e classes para a informação dos comentarios.

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140720204929) do
+ActiveRecord::Schema.define(version: 20140824180208) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20140720204929) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
